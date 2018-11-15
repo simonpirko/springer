3df0987e65355e6ccfc5c2adb62e6986590a2d1c
Simon Pirko <sipi7gm@gmail.com>
2018-11-15 13:29:31 +0300

    Update rep.

--- annspring/src/main/java/com/spring/jpa/MainJpa.java
+++ annspring/src/main/java/com/spring/jpa/MainJpa.java
@@ -9,7 +9,8 @@ public class MainJpa {
         GenericXmlApplicationContext genericXmlApplicationContext = new GenericXmlApplicationContext();
         genericXmlApplicationContext.load("app-conf.xml");
         genericXmlApplicationContext.refresh();
-        PersonServiceImpl personService = genericXmlApplicationContext.getBean("personService", PersonServiceImpl.class);
+        PersonServiceImpl personService = genericXmlApplicationContext
+                .getBean("personService", PersonServiceImpl.class);
         Person person = new Person();
         person.setName("Simon");
         person.setAge(24);
