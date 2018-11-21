d452786fb6d28f0538eaaccd4fba1ee378000fee
Simon Pirko <sipi7gm@gmail.com>
2018-11-21 03:40:20 +0300

    Update modules.

--- xmlspring/src/main/java/com/springer/jdbc/MainJdbcXml.java
+++ xmlspring/src/main/java/com/springer/jdbc/MainJdbcXml.java
@@ -3,8 +3,20 @@ package com.springer.jdbc;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.ArrayList;
+
 public class MainJdbcXml {
+    private ArrayList<Integer> arrayList;
+
+    public MainJdbcXml() {
+        arrayList = new ArrayList<>();
+    }
+
     public static void main(String[] args) {
+        MainJdbcXml mainJdbcXml = new MainJdbcXml();
+        mainJdbcXml.arrayList.add(2);
+        String helloMyname = "Simon";
+        System.out.println(helloMyname);
         ApplicationContext applicationContext =
                 new ClassPathXmlApplicationContext("application-context.xml");
         JdbcContactDao jdbcContactDao = applicationContext.getBean("jdbcContactDao", JdbcContactDao.class);
